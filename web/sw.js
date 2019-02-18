867a07cd2015d24812529138b64c4f91388c6cd7
Dani Roxberry <dani@bitoriented.com>
2019-02-18 15:31:30 -0800

    [add] cleanup docsify deployment; make work offline

--- web/sw.js
+++ web/sw.js
@@ -11,7 +11,9 @@ const HOSTNAME_WHITELIST = [
   self.location.hostname,
   'fonts.gstatic.com',
   'fonts.googleapis.com',
-  'unpkg.com'
+  'unpkg.com',
+  'github.com',
+  'github.io'
 ]
 
 // The Util Function to hack URLs of intercepted requests
